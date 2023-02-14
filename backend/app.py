af55d766805dfa2416e2dcdff90250c21c740107
bengoodheart <bgoodheart@fordham.edu>
2023-02-14 17:56:43 -0500

    more init

---
+++ backend/app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from dotenv import load_dotenv
+import os
+from flask_cors import CORS
+from flask_migrate import Migrate
+
+#Model Imports
+from models.db_init import db
+
+app = Flask(__name__)
+load_dotenv()
+CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/appTable.db'
+migrate = Migrate(app, db)
+
+@app.route('/')
+def home():
+    return "HELLO WORLD"
+
+@app.route('/site/info')
+def info():
+    info = {
+        "site.name" : "qu-sports-app",
+        "version": "v0.0.1",
+        "status" : "development",
+    }
+    
+    return info
